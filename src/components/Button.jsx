c568eb7cc07332b2b0f267051280dc752c629c88
aliserhan315 <aliserhan315@gmail.com>
2024-11-22 11:10:28 +0200

    added details and button styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,32 +1,34 @@
 const Button = ({
-    label,
-    iconURL,
-    backgroundColor,
-    textColor,
-    borderColor,
-    fullWidth,
-    Onclick
-  }) => {
-    return (
-      <button
-        className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none transition-transform duration-300 ease-in-out
         ${
           backgroundColor
             ? `${backgroundColor} ${textColor} ${borderColor}`
             : "bg-coral-red text-white border-coral-red"
-        } rounded-full ${fullWidth && "w-full"}`} 
-       Onclick={Onclick}>
-        {label}
-  
-        {iconURL && (
-          <img
-            src={iconURL}
-            alt='arrow right icon'
-            className='ml-2 rounded-full bg-white w-5 h-5'
-          />
-        )}
-      </button>
-    );
-  };
-  
-  export default Button;
+        } rounded-full ${fullWidth ? "w-full" : ""} 
+        hover:scale-105 hover:shadow-md active:scale-95`} // Hover and click effects
+    >
+      {label}
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
